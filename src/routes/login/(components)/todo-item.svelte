64e7af30a4d6722f0e3639ab3cca268b6278a495
Akeanant Poomdeesittinon <aekanan2004@gmail.com>
2024-03-31 10:22:49 +0700

    setup page

---
+++ src/routes/login/(components)/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="">
+        <input type="hidden" name="done" value="" id="">
+        <button aria-label="Mark done/not done">Done/NotDone</button>
+    </form>
+
+    <form action="">
+        <input type="text" name="" id="">
+        <button aria-label="Save todo">Save</button>
+    </form>
+
+    <form action="">
+        <button aria-label="Delete todo">Delele</button>
+    </form>
+</div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Item from "./(components)/todo-item.svelte";
+    const  title = "Todo | New";
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+
+<form action="" method="">
+    <input type="text" name="text" id="" placeholder="type to do" />
+    <Item />
+</form>
+
+<Item />
+<Item />
+<Item />
+
+
